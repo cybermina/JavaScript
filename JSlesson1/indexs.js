4e2b8cb06eea8c7c842a56736766f2262aa45307
Baxromova_Nozanin <pozetivmiss@gmail.com>
2025-10-29 14:24:49 +0500

    Add files via upload

--- JSlesson1/indexs.js
+++ JSlesson1/indexs.js
@@ -1,51 +1,51 @@
 //1-mashq
-let soat = prompt("Soatni kiriting: ");
-let daqiqa = soat * 60;
+var soat = prompt("Soatni kiriting: ");
+var daqiqa = soat * 60;
 alert("Daqiqalqr soni: " + daqiqa)
 //2-mashq
-let ism = prompt("Ismingizni kiriting:");
-let yosh = prompt("Yoshingizni kiriting:");
+var ism = prompt("Ismingizni kiriting:");
+var yosh = prompt("Yoshingizni kiriting:");
 alert("Salom, " + ism + "! Sizning yoshingiz: " + yosh);
 //3-mashq
-let name = "Pororo";
-let age = 18;
-let talaba = true;
+var name = "Pororo";
+var age = 18;
+var talaba = true;
 console.log("name turi:", typeof name);
 console.log("age turi:", typeof age);
 console.log("talaba turi:", typeof talaba);
 //4-mashq
-let son1 = prompt("1-sonni kiriting:");
-let son2 = prompt("2-sonni kiriting:");
-let yigindi = Number(son1) + Number(son2);
+var son1 = prompt("1-sonni kiriting:");
+var son2 = prompt("2-sonni kiriting:");
+var yigindi = Number(son1) + Number(son2);
 alert("Yig‘indi: " + yigindi);
 //5-mashq
-let a = prompt("1-sonni kiriting:");
-let b = prompt("2-sonni kiriting:");
-let c = prompt("3-sonni kiriting:");
-let orta = (Number(a) + Number(b) + Number(c)) / 3;
+var a = prompt("1-sonni kiriting:");
+var b = prompt("2-sonni kiriting:");
+var c = prompt("3-sonni kiriting:");
+var orta = (Number(a) + Number(b) + Number(c)) / 3;
 console.log("O‘rta arifmetik:", orta);
 //6-mashq
-let s1 = prompt("1-son:");
-let s2 = prompt("2-son:");
+var s1 = prompt("1-son:");
+var s2 = prompt("2-son:");
 console.log("Ko‘paytma:", s1 * s2);
 
 // 7-mashq
-let n1 = prompt("1-son:");
-let n2 = prompt("2-son:");
+var n1 = prompt("1-son:");
+var n2 = prompt("2-son:");
 console.log("Bo‘linma:", n1 / n2);
 //8-mashq
-let x1 = prompt("1-son:");
-let x2 = prompt("2-son:");
-let kvadratYigindi = x1**2 + x2**2;
+var x1 = prompt("1-son:");
+var x2 = prompt("2-son:");
+var kvadratYigindi = x1**2 + x2**2;
 console.log("Kvadratlar yig‘indisi:", kvadratYigindi);
 //9-mashq
-let y1 = prompt("1-son:");
-let y2 = prompt("2-son:");
-let kubFarqi = y1**3 - y2**3;
+var y1 = prompt("1-son:");
+var y2 = prompt("2-son:");
+var kubFarqi = y1**3 - y2**3;
 console.log("Kublar farqi:", kubFarqi);
 //10-mashq
-let z1 = prompt("1-son:");
-let z2 = prompt("2-son:");
-let kubKopaytma = (z1**3) * (z2**3);
+var z1 = prompt("1-son:");
+var z2 = prompt("2-son:");
+var kubKopaytma = (z1**3) * (z2**3);
 console.log("Kublar ko‘paytmasi:", kubKopaytma);
 
