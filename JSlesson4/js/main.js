b7209ca7f81ba0e35c99459c82bbda11cae219c8
Baxromova_Nozanin <pozetivmiss@gmail.com>
2025-11-04 20:06:33 +0500

    Add files via upload

---
+++ JSlesson4/js/main.js
@@ -0,0 +1,68 @@
+//AMALIY
+//1-mashq
+// let arr = [1, 2, 2, 3, 3, 4];
+// let unique = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//  let a = true;
+//  for (let j = 0; j < unique.length; j++) {
+//    if (arr[i] == unique[j]) a = false;
+//  }
+//  if (a) unique.push(arr[i]);
+// }
+// console.log(unique); // [1, 2, 3, 4]
+
+//2-mashq
+// let arr = [5, 2, 9, 1, 7];
+// let min = arr[0];
+
+// for (let i = 1; i < arr.length; i++) {
+//  if (arr[i] < min) min = arr[i];
+// }
+// console.log(min); // 1
+
+//Home Work
+//1-mashq
+// let num = 12345;
+// let sum = 0;
+
+// while (num > 0) {
+//  sum = sum + (num % 10);  
+//  num = Math.floor(num / 10); 
+// }
+// console.log(sum);
+
+//2-mashq
+// let str = "salom hammaga nima gaplar qalay";
+// let word = "";
+
+// for (let i = 0; i <= str.length; i++) {
+//  if (str[i] == " " || i == str.length) {
+//    console.log(word + " = " + word.length);
+//    word = "";
+//  } else {
+//    word = word + str[i];
+//  }
+// }
+
+//3-mashq
+// let arr = [1,2,3,4,5,6,7,8,9];
+// let even = [];
+// for(let i=0;i<arr.length;i++){
+//  if(arr[i]%2==0) a.push(arr[i]);
+// }
+// console.log(a); 
+
+//4-mashq
+// let arr = [1,2,3,4,5,6,7,8,9];
+// let odd = [];
+// for(let i=0;i<arr.length;i++){
+//  if(arr[i]%2!=0) b.push(arr[i]);
+// }
+// console.log(b);
+
+
+
+
+
+
